ac1a94d5a80dfdafcdad3a876e9d348a62e2e4eb
rifadhossainrabbi <hossainmrrifad@gmail.com>
2026-04-30 23:36:48 +0600

    Add filter by Category

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -7,11 +7,3 @@ export async function getBooks() {
   return data;
 }
 
-// export async function getBookById() {
-//   const res = await fetch(
-//     `https://book-borrowing-platform-next-js-por.vercel.app/data.json/${bookId}`,
-//     { cache: 'no-store' },
-//   );
-//   const data = await res.json();
-//   return data;
-// }
